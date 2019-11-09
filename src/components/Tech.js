13b3afb01cf439f1455967b4e997b10bdfeef3e9
Rusty Abbott <rusty@hutchinson-island.com>
2019-11-09 14:45:33 -0500

    .gif background and layout changes

---
+++ src/components/Tech.js
@@ -0,0 +1,10 @@
+import { React, Component } from 'react';
+import { Link, Route } from 'react-router-dom';
+
+const Technology = ({ match }) => {
+  return(
+    <div>Testing technology</div>
+  )
+}
+
+export default Technology;
