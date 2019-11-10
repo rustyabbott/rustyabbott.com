ba46e9c994185c276b624918613619d4faff7a6e
Rusty Abbott <rusty@hutchinson-island.com>
2019-11-09 20:04:56 -0500

    Make side navigation drawer functional

--- src/App.js
+++ src/App.js
@@ -1,36 +1,63 @@
 import React from 'react';
 import './App.css';
-import { Route, Link, Switch } from "react-router-dom";
+import { Switch, Route, Link } from 'react-router-dom';
 import Tech from './components/Tech';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <div className="App-header-container">
-          <div className="App-name">
-            Rusty Abbott
+class App extends React.Component {
+  state = {
+    open: false
+  }
+
+  componentDidMount = () => {
+    this.setState({
+      ...this.state
+    });
+  }
+
+  toggleDrawer = () => {
+    const drawer = document.getElementById('Drawer');
+
+    this.setState({
+      open: !this.state.open
+    });
+    this.state.open ? drawer.classList.remove('visible') : drawer.classList.add('visible');
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <div className="App-header-container">
+            <div className="App-name">
+              Rusty Abbott
+            </div>
+            <button className="Burger" onClick={ this.toggleDrawer }>
+              <svg viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg"><path d="M1664 1344v128q0 26-19 45t-45 19H192q-26 0-45-19t-19-45v-128q0-26 19-45t45-19h1408q26 0 45 19t19 45zm0-512v128q0 26-19 45t-45 19H192q-26 0-45-19t-19-45V832q0-26 19-45t45-19h1408q26 0 45 19t19 45zm0-512v128q0 26-19 45t-45 19H192q-26 0-45-19t-19-45V320q0-26 19-45t45-19h1408q26 0 45 19t19 45z"></path></svg>
+            </button>
           </div>
-          <div className="Burger">
-            <svg viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg"><path d="M1664 1344v128q0 26-19 45t-45 19H192q-26 0-45-19t-19-45v-128q0-26 19-45t45-19h1408q26 0 45 19t19 45zm0-512v128q0 26-19 45t-45 19H192q-26 0-45-19t-19-45V832q0-26 19-45t45-19h1408q26 0 45 19t19 45zm0-512v128q0 26-19 45t-45 19H192q-26 0-45-19t-19-45V320q0-26 19-45t45-19h1408q26 0 45 19t19 45z"></path></svg>
+        </header>
+        <main>
+          <div className="Drawer" id="Drawer">
+            <ul>
+              <li><Link to='/'>Home</Link></li>
+              <li><Link to='tech'>Tech</Link></li>
+            </ul>
+            <Switch>
+              <Route exact path={ '/tech' } component={ Tech } />
+              <Route exact path={ '/' } />
+            </Switch>
           </div>
-        </div>
-      </header>
-      <main>
-        <Link to='tech'>Tech</Link>
-        <Switch>
-          <Route exact path={ '/tech' } component={ Tech } />
-        </Switch>
-      </main>
-      <footer>
-        <div className="Social">
-          <a href="https://github.com/rustyabbott">
-            <img src="img/github-icon.svg" alt="GitHub" />
-          </a>
-        </div>
-      </footer>
-    </div>
-  );
+        </main>
+        <footer>
+          <div className="Social">
+            <a href="https://github.com/rustyabbott">
+              <img src="img/github-icon.svg" alt="GitHub" />
+            </a>
+          </div>
+        </footer>
+      </div>
+    );
+  }
 }
 
 export default App;
