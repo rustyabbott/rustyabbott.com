523e1c91b154de28f46b4c1b6c2dbfaef68ccb32
Rusty Abbott <rusty@hutchinson-island.com>
2019-11-09 21:46:39 -0500

    Center menu with circle style

--- src/App.js
+++ src/App.js
@@ -14,13 +14,13 @@ class App extends React.Component {
     });
   }
 
-  toggleDrawer = () => {
-    const drawer = document.getElementById('Drawer');
+  toggleNav = () => {
+    const nav = document.getElementById('nav');
 
     this.setState({
       open: !this.state.open
     });
-    this.state.open ? drawer.classList.remove('visible') : drawer.classList.add('visible');
+    this.state.open ? nav.classList.remove('visible') : nav.classList.add('visible');
   }
 
   render() {
@@ -31,22 +31,20 @@ class App extends React.Component {
             <div className="App-name">
               Rusty Abbott
             </div>
-            <button className="Burger" onClick={ this.toggleDrawer }>
+            <button className="Burger" onClick={ this.toggleNav }>
               <svg viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg"><path d="M1664 1344v128q0 26-19 45t-45 19H192q-26 0-45-19t-19-45v-128q0-26 19-45t45-19h1408q26 0 45 19t19 45zm0-512v128q0 26-19 45t-45 19H192q-26 0-45-19t-19-45V832q0-26 19-45t45-19h1408q26 0 45 19t19 45zm0-512v128q0 26-19 45t-45 19H192q-26 0-45-19t-19-45V320q0-26 19-45t45-19h1408q26 0 45 19t19 45z"></path></svg>
             </button>
           </div>
         </header>
         <main>
-          <div className="Drawer" id="Drawer">
-            <ul>
-              <li><Link to='/'>Home</Link></li>
-              <li><Link to='tech'>Tech</Link></li>
-            </ul>
-            <Switch>
-              <Route exact path={ '/tech' } component={ Tech } />
-              <Route exact path={ '/' } />
-            </Switch>
+          <div className="Nav" id="nav">
+            <Link to='/' onClick={ this.toggleNav }><div>Home</div></Link>
+            <Link to='tech'onClick={ this.toggleNav }><div>Tech</div></Link>
           </div>
+          <Switch>
+            <Route exact path={ '/tech' } component={ Tech } />
+            <Route exact path={ '/' } />
+          </Switch>
         </main>
         <footer>
           <div className="Social">
