115fd6bcd47108295e20487d2eda33c7931d28da
Rusty Abbott <rusty@hutchinson-island.com>
2019-11-10 21:49:57 -0500

    Create first breakpoint

--- src/App.js
+++ src/App.js
@@ -28,19 +28,20 @@ class App extends React.Component {
       <div className="App" id="App">
         <header className="App-header">
           <div className="App-header-container">
-            <Link to="/">
-              <div className="App-name">Rusty Abbott</div>
-            </Link>
+            <span className="App-name"><Link to="/">Home</Link></span>
             <button className="Burger" onClick={ this.toggleNav }>
               <svg viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg"><path d="M1664 1344v128q0 26-19 45t-45 19H192q-26 0-45-19t-19-45v-128q0-26 19-45t45-19h1408q26 0 45 19t19 45zm0-512v128q0 26-19 45t-45 19H192q-26 0-45-19t-19-45V832q0-26 19-45t45-19h1408q26 0 45 19t19 45zm0-512v128q0 26-19 45t-45 19H192q-26 0-45-19t-19-45V320q0-26 19-45t45-19h1408q26 0 45 19t19 45z"></path></svg>
             </button>
+            <div className="Pages">
+              <span><Link to="/tech">Technology</Link></span>
+            </div>
           </div>
         </header>
         <main>
-          <div className="Nav" id="nav">
+          <nav className="Nav" id="nav">
             <Link to='/' onClick={ this.toggleNav }><button className="navButton">Home</button></Link>
             <Link to='tech'onClick={ this.toggleNav }><button className="navButton">Tech</button></Link>
-          </div>
+          </nav>
           <Switch>
             <Route exact path={ '/tech' } component={ Tech } />
             <Route exact path={ '/' } />
