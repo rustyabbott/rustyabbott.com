c40209a73fb7e35536873ece83f0f00199201fc6
Rusty Abbott <rusty@hutchinson-island.com>
2019-11-11 20:35:03 -0500

    Minor style adjustments

--- src/components/Tech.js
+++ src/components/Tech.js
@@ -4,10 +4,13 @@ export default class Tech extends React.Component {
   render() {
     return (
       <div className="pageWrapper">
-        <h1>Technology</h1>
+        <h1>&lt;Technology /&gt;</h1>
         <h2>Software & languages</h2>
         <p>React, JavaScript / jQuery / ES6, PHP, MySQL, MariaDB, Python, Bash, HTML5, CSS3, SASS, Symfony 2, Symfony 4, Linux, Apache, Node.js, Adobe Suite, AWS.</p>
-        <h2>Projects</h2>
+        <h2>RustyAbbott.com</h2>
+        <p>This web application was built with Facebook's <a href="https://github.com/facebook/create-react-app" target="_blank" rel="noopener noreferrer">Create React App</a>.</p>
+        <h2>Hutchinson-Island.com</h2>
+        <p>This project was originally built with PHP, jQuery, MySQL, HTML, & CSS, then later rebuilt with Symfony 4.</p>
       </div>
     );
   }
