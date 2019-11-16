40f6d0a0044e9332f4ec6c8258db48cae1000fe9
Rusty Abbott <rusty@hutchinson-island.com>
2019-11-16 00:24:01 -0500

    Style links and underline active page in header

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import Tech from './components/Tech';
 
 class App extends React.Component {
   state = {
-    open: false
+    open: false,
+    pathname: window.location.pathname
   }
 
   componentDidMount = () => {
@@ -16,11 +17,36 @@ class App extends React.Component {
 
   toggleNav = () => {
     const nav = document.getElementById('nav');
-
     this.setState({
+      ...this.state,
       open: !this.state.open
     });
     this.state.open ? nav.classList.remove('visible') : nav.classList.add('visible');
+  }
+
+  pageActive = () =>  {
+    const pathname = window.location.pathname;
+    this.setState({
+      ...this.state,
+      pathname: pathname
+    })
+    const tech = document.getElementById('tech');
+    const home = document.getElementById('home');
+    console.log(pathname);
+    console.log(tech);
+    console.log(home);
+
+    switch(pathname) {
+      case '/tech':
+        tech.classList.add('pageActive');
+        home.classList.remove('pageActive');
+        break;
+      case '/':
+        home.classList.add('pageActive');
+        tech.classList.remove('pageActive');
+        break;
+      default:
+    }
   }
 
   render() {
@@ -28,12 +54,12 @@ class App extends React.Component {
       <div className="App" id="App">
         <header className="App-header">
           <div className="App-header-container">
-            <span className="App-name"><Link to="/">Home</Link></span>
+            <span className="App-name"><Link to="/" id="home" onClick={ this.pageActive }>Home</Link></span>
             <button className="Burger" onClick={ this.toggleNav }>
               <svg viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg"><path d="M1664 1344v128q0 26-19 45t-45 19H192q-26 0-45-19t-19-45v-128q0-26 19-45t45-19h1408q26 0 45 19t19 45zm0-512v128q0 26-19 45t-45 19H192q-26 0-45-19t-19-45V832q0-26 19-45t45-19h1408q26 0 45 19t19 45zm0-512v128q0 26-19 45t-45 19H192q-26 0-45-19t-19-45V320q0-26 19-45t45-19h1408q26 0 45 19t19 45z"></path></svg>
             </button>
             <div className="Pages">
-              <span><Link to="/tech">Technology</Link></span>
+              <span><Link to="/tech" onClick={ this.pageActive } id="tech">Technology</Link></span>
             </div>
           </div>
         </header>
--- src/components/Tech.js
+++ src/components/Tech.js
@@ -5,8 +5,8 @@ export default class Tech extends React.Component {
     return (
       <div className="pageWrapper">
         <h1>&lt;Technology /&gt;</h1>
-        <h2>Software & languages</h2>
-        <p>React, JavaScript / jQuery / ES6, PHP, MySQL, MariaDB, Python, Bash, HTML5, CSS3, SASS, Symfony 2, Symfony 4, Linux, Apache, Node.js, Adobe Suite, AWS.</p>
+        <h2>Software & Languages</h2>
+        <p>React, JavaScript / jQuery / ES6, PHP, MySQL, MariaDB, Python, Bash, HTML5, CSS3, SASS, Symfony 2, Symfony 4, Linux, Apache, Node.js, Yarn, npm, Webpack, Adobe Suite, AWS.</p>
         <h2>RustyAbbott.com</h2>
         <p>This web application was built with Facebook's <a href="https://github.com/facebook/create-react-app" target="_blank" rel="noopener noreferrer">Create React App</a>.</p>
         <h2>Hutchinson-Island.com</h2>
