e16b79096e53c66580d5a4d967b180025c842c0f
Rusty Abbott <rusty@hutchinson-island.com>
2019-11-17 12:57:17 -0500

    Modify active page underline functions

--- src/components/Tech.js
+++ src/components/Tech.js
@@ -6,7 +6,7 @@ export default class Tech extends React.Component {
       <div className="pageWrapper">
         <h1>&lt;Technology /&gt;</h1>
         <h2>Software & Languages</h2>
-        <p>React, JavaScript / jQuery / ES6, PHP, MySQL, MariaDB, Python, Bash, HTML5, CSS3, SASS, Symfony 2, Symfony 4, Linux, Apache, Node.js, Yarn, npm, Webpack, Adobe Suite, AWS.</p>
+        <p>React, JavaScript / jQuery / ES6, PHP, MySQL, MariaDB, Python, Bash, HTML5, CSS3, SASS, Symfony 2, Symfony 4, Linux, Apache, Node.js, Yarn, npm, Webpack, Adobe Suite, AWS, Git, GitHub, GitLab.</p>
         <h2>RustyAbbott.com</h2>
         <p>This web application was built with Facebook's <a href="https://github.com/facebook/create-react-app" target="_blank" rel="noopener noreferrer">Create React App</a>.</p>
         <h2>Hutchinson-Island.com</h2>
