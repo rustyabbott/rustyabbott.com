dc0b968753629320dcb092e158de3a6ae8bcc6d9
Rusty Abbott <rusty@hutchinson-island.com>
2019-12-22 16:03:54 -0500

    Test deployment with AWS Amplify

--- src/components/Tech.js
+++ src/components/Tech.js
@@ -14,9 +14,9 @@ export default class Tech extends React.Component {
         <h2>Software & Languages</h2>
         <p>React, JavaScript / jQuery / ES6, PHP, MySQL, MariaDB, Python, Bash, HTML5, CSS3, SASS, Symfony 2, Symfony 4, Linux, Apache, Node.js, Yarn, npm, Webpack, Adobe Suite, AWS, Git, GitHub, GitLab.</p>
         <h2>RustyAbbott.com</h2>
-        <p>This web application was built with Facebook's <a href="https://github.com/facebook/create-react-app" target="_blank" rel="noopener noreferrer">Create React App</a>. It utilizes react-router-dom for routing and SASS for styling.</p>
+        <p>The web app you're currently viewing was built with Facebook's <a href="https://github.com/facebook/create-react-app" target="_blank" rel="noopener noreferrer">Create React App</a>. It utilizes react-router-dom for routing and SASS/SCSS for styling. I'm currently experimenting with AWS Amplify for deployment.</p>
         <h2>Hutchinson-Island.com</h2>
-        <p>This project was originally built with PHP, jQuery, MySQL, HTML, & CSS, then later rebuilt with Symfony 4.</p>
+        <p>Coming soon! This project is currently being rebuilt with Symfony 4.</p>
         <h2>Memory Game (<a href="https://github.com/rustyabbott/memory-game" target="_blank" rel="noopener noreferrer">GitHub</a>, <a href="https://rustyabbott.github.io/memory-game/" target="_blank" rel="noopener noreferrer">Play Live</a>)</h2>
         <p>Match all cards to win! Project two for the Grow with Google / Udacity front-end web development program. Coded with jQuery.</p>
         <div className="gif-wrapper">
@@ -32,8 +32,6 @@ export default class Tech extends React.Component {
         <div className="gif-wrapper">
           <img src="img/analog-clock.gif" alt="arcade game" />
         </div>
-        <h3>Neighborhood Map</h3>
-        <h3>Restaurant Reviews</h3>
       </div>
     );
   }
