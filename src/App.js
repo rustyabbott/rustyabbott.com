886442c53bd1e48ea28d8ab4970aa625b0ba20c0
Rusty Abbott <rusty@hutchinson-island.com>
2019-12-29 19:21:57 -0500

    Refactor to run homeActive & techActive after DOM loads

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import { Switch, Route, Link } from 'react-router-dom';
 import Tech from './components/Tech';
 
-class App extends React.Component {
+const path = window.location.pathname;
+
+const homeActive = () =>  {
+  const home = document.getElementById('home');
+  const tech = document.getElementById('tech');
+  const footer = document.getElementById('footer');
+  home.classList.add('pageActive');
+  tech.classList.remove('pageActive');
+  footer.classList.add('bottom');
+}
+
+const techActive = () => {
+  const home = document.getElementById('home');
+  const tech = document.getElementById('tech');
+  const footer = document.getElementById('footer');
+  home.classList.remove('pageActive');
+  tech.classList.add('pageActive');
+  footer.classList.remove('bottom');
+}
+
+window.addEventListener('DOMContentLoaded', (event) => {
+    if (path === '/') {
+      homeActive();
+    } else if (path === '/tech') {
+      techActive();
+    }
+});
+
+export default class App extends Component {
   constructor (props) {
     console.log('constructor');
     super(props);
@@ -14,9 +42,6 @@ class App extends React.Component {
   }
 
   componentDidMount = () => {
-    this.setState({
-      ...this.state
-    });
     console.log('home mounted');
   }
 
@@ -32,24 +57,6 @@ class App extends React.Component {
     this.state.open ? nav.classList.remove('visible') : nav.classList.add('visible');
   }
 
-  homeActive = () =>  {
-    const home = document.getElementById('home');
-    const tech = document.getElementById('tech');
-    const footer = document.getElementById('footer');
-    home.classList.add('pageActive');
-    tech.classList.remove('pageActive');
-    footer.classList.add('bottom');
-  }
-
-  techActive = () => {
-    const home = document.getElementById('home');
-    const tech = document.getElementById('tech');
-    const footer = document.getElementById('footer');
-    home.classList.remove('pageActive');
-    tech.classList.add('pageActive');
-    footer.classList.remove('bottom');
-  }
-
   render() {
     console.log('home render');
 
@@ -57,12 +64,12 @@ class App extends React.Component {
       <div className="App" id="App">
         <header className="App-header">
           <div className="App-header-container">
-            <span className="App-name"><Link to="/" id="home" onClick={ this.homeActive }>Home</Link></span>
+            <span className="App-name"><Link to="/" id="home" onClick={ homeActive }>Home</Link></span>
             <button className="Burger" onClick={ this.toggleNav }>
               <svg viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg"><path d="M1664 1344v128q0 26-19 45t-45 19H192q-26 0-45-19t-19-45v-128q0-26 19-45t45-19h1408q26 0 45 19t19 45zm0-512v128q0 26-19 45t-45 19H192q-26 0-45-19t-19-45V832q0-26 19-45t45-19h1408q26 0 45 19t19 45zm0-512v128q0 26-19 45t-45 19H192q-26 0-45-19t-19-45V320q0-26 19-45t45-19h1408q26 0 45 19t19 45z"></path></svg>
             </button>
             <div className="Pages">
-              <span><Link to="/tech" onClick={ this.techActive } id="tech">Technology</Link></span>
+              <span><Link to="/tech" onClick={ techActive } id="tech">Technology</Link></span>
             </div>
           </div>
         </header>
@@ -87,5 +94,3 @@ class App extends React.Component {
     );
   }
 }
-
-export default App;
