0d596d0ee2cb2a792e5fb4a65c7810bd66a1139f
Rusty Abbott <rusty@hutchinson-island.com>
2020-01-06 22:07:47 -0500

    Rewrite to handle pageActive by state

--- src/App.js
+++ src/App.js
@@ -3,45 +3,58 @@ import './App.css';
 import { Switch, Route, Link } from 'react-router-dom';
 import Tech from './components/Tech';
 
-const path = window.location.pathname;
-
-const homeActive = () =>  {
-  document.getElementById('home').classList.add('pageActive');
-  document.getElementById('tech').classList.remove('pageActive');
-  document.getElementById('footer').classList.add('bottom');
-}
-
-const techActive = () => {
-  document.getElementById('home').classList.remove('pageActive');
-  document.getElementById('tech').classList.add('pageActive');
-  document.getElementById('footer').classList.remove('bottom');
-}
-
-const pathSwitch = () => {
-  if (path === '/') {
-    homeActive();
-  } else if (path === '/tech') {
-    techActive();
-  }
-}
-
-window.addEventListener('DOMContentLoaded', (event) => {
-  pathSwitch();
-});
-
 export default class App extends Component {
   state = {
     open: false,
-    pageActive: 'home'
+    pageActive: window.location.pathname
+  }
+
+  activeSwitch = () => {
+    if (this.state.pageActive === '/') {
+      document.getElementById('home').classList.add('pageActive');
+      document.getElementById('tech').classList.remove('pageActive');
+      document.getElementById('footer').classList.add('bottom');
+    } else if (this.state.pageActive === '/tech') {
+      document.getElementById('home').classList.remove('pageActive');
+      document.getElementById('tech').classList.add('pageActive');
+      document.getElementById('footer').classList.remove('bottom');
+    }
   }
 
   componentDidMount = () => {
     console.log('home mounted');
+    this.activeSwitch();
   }
 
   componentDidUpdate = () => {
     console.log('home updated');
-    pathSwitch();
+    this.activeSwitch();
+  }
+
+  homeActive = () => {
+    this.setState({
+      pageActive: '/'
+    });
+  }
+
+  techActive = () => {
+    this.setState({
+      pageActive: '/tech'
+    });
+  }
+
+  mobileHomeActive = () => {
+    this.setState({
+      pageActive: '/'
+    });
+    this.toggleNav();
+  }
+
+  mobileTechActive = () => {
+    this.setState({
+      pageActive: '/tech'
+    });
+    this.toggleNav();
   }
 
   toggleNav = () => {
@@ -59,19 +72,19 @@ export default class App extends Component {
       <div className="App" id="App">
         <header className="App-header">
           <div className="App-header-container">
-            <span className="App-name"><Link to="/" id="home" onClick={ homeActive }>Home</Link></span>
+            <span className="App-name"><Link to="/" id="home" onClick={ this.homeActive }>Home</Link></span>
             <button className="Burger" onClick={ this.toggleNav }>
               <svg viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg"><path d="M1664 1344v128q0 26-19 45t-45 19H192q-26 0-45-19t-19-45v-128q0-26 19-45t45-19h1408q26 0 45 19t19 45zm0-512v128q0 26-19 45t-45 19H192q-26 0-45-19t-19-45V832q0-26 19-45t45-19h1408q26 0 45 19t19 45zm0-512v128q0 26-19 45t-45 19H192q-26 0-45-19t-19-45V320q0-26 19-45t45-19h1408q26 0 45 19t19 45z"></path></svg>
             </button>
             <div className="Pages">
-              <span><Link to="/tech" onClick={ techActive } id="tech">Technology</Link></span>
+              <span><Link to="/tech" onClick={ this.techActive } id="tech">Technology</Link></span>
             </div>
           </div>
         </header>
         <main>
           <nav className="Nav" id="nav">
-            <Link to='/' onClick={ this.toggleNav }><button className="navButton">Home</button></Link>
-            <Link to='tech' onClick={ this.toggleNav }><button className="navButton">Tech</button></Link>
+            <Link to='/' onClick={ this.mobileHomeActive }><button className="navButton">Home</button></Link>
+            <Link to='tech' onClick={ this.mobileTechActive }><button className="navButton">Tech</button></Link>
           </nav>
           <Switch>
             <Route exact path={ '/tech' } component={ Tech } />
