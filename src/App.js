1f9d43fc6e3974066335fe4b5b113fd0b46821b3
Rusty Abbott <dev@rustyabbott.com>
2020-02-29 14:18:22 -0500

    Switch from React Router to HookRouter

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import './css/App.css';
-import { Switch, Route, Link } from 'react-router-dom';
-import Tech from './components/Tech';
-import Sort from './components/Sort';
-import { useRoutes } from 'hookrouter';
+// import { Switch, Route, Link } from 'react-router-dom';
+import { useRoutes, A } from 'hookrouter';
+import Routes from './components/Routes';
 
 const App = () => {
+  const routeResult = useRoutes(Routes);
   const [pageActive, setPageActive] = useState(window.location.pathname);
   const [open, setOpen] = useState(false);
 
@@ -54,25 +54,20 @@ const App = () => {
     <div className="App" id="App">
       <header className="App-header">
         <div className="App-header-container">
-          <span className="App-name"><Link to="/" id="home" onClick={ homeActive }>Home</Link></span>
+          <span className="App-name"><A href="/" id="home" onClick={ homeActive }>Home</A></span>
           <button className="Burger" onClick={ toggleNav }>
             <svg viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg"><path d="M1664 1344v128q0 26-19 45t-45 19H192q-26 0-45-19t-19-45v-128q0-26 19-45t45-19h1408q26 0 45 19t19 45zm0-512v128q0 26-19 45t-45 19H192q-26 0-45-19t-19-45V832q0-26 19-45t45-19h1408q26 0 45 19t19 45zm0-512v128q0 26-19 45t-45 19H192q-26 0-45-19t-19-45V320q0-26 19-45t45-19h1408q26 0 45 19t19 45z"></path></svg>
           </button>
           <div className="Pages">
-            <span><Link to="/tech" onClick={ techActive } id="tech">Technology</Link></span>
+            <span><A href="/tech" onClick={ techActive } id="tech">Technology</A></span>
           </div>
         </div>
       </header>
       <main>
         <nav className="Nav" id="nav">
-          <Link to='/' onClick={ mobileHomeActive }><button className="navButton">Home</button></Link>
-          <Link to='tech' onClick={ mobileTechActive }><button className="navButton">Tech</button></Link>
+          <A href='/' onClick={ mobileHomeActive }><button className="navButton">Home</button></A>
+          <A href='tech' onClick={ mobileTechActive }><button className="navButton">Tech</button></A>
         </nav>
-        <Switch>
-          <Route exact path={ '/tech' } component={ Tech } />
-          <Route exact path={ '/' } />
-          <Route exat path={ '/sort' } component={ Sort } />
-        </Switch>
       </main>
       <footer className="bottom" id="footer">
         <div className="Social">
@@ -81,6 +76,7 @@ const App = () => {
           </a>
         </div>
       </footer>
+      { routeResult }
     </div>
   );
 }
---
+++ src/components/Routes.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Tech from './Tech';
+import Sort from './Sort';
+
+const Routes = {
+  "/tech": () => <Tech />,
+  "/sort": () => <Sort />
+};
+
+export default Routes;
