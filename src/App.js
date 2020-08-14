693fb760ba6b67fd6cdff8c7821fdde172020e1f
Rusty Abbott <dev@rustyabbott.com>
2020-08-13 20:53:49 -0400

    Change home text link to house icon for mobile view

--- src/App.js
+++ src/App.js
@@ -61,7 +61,10 @@ const App = () => {
     <div className="App" id="App">
       <header className="App-header">
         <div className="App-header-container">
-          <span className="App-name"><A href="/" id="home" onClick={ homeActive }>Home</A></span>
+          <A href="/" className="HomeIconWrapper" onClick={ homeActive }>
+            <img src="/img/home-icon.png" alt="home icon" />
+          </A>
+          <span className="HomeText"><A href="/" id="home" onClick={ homeActive }>Home</A></span>
           <button className="Burger" onClick={ toggleNav }>
             <svg viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg"><path d="M1664 1344v128q0 26-19 45t-45 19H192q-26 0-45-19t-19-45v-128q0-26 19-45t45-19h1408q26 0 45 19t19 45zm0-512v128q0 26-19 45t-45 19H192q-26 0-45-19t-19-45V832q0-26 19-45t45-19h1408q26 0 45 19t19 45zm0-512v128q0 26-19 45t-45 19H192q-26 0-45-19t-19-45V320q0-26 19-45t45-19h1408q26 0 45 19t19 45z"></path></svg>
           </button>
